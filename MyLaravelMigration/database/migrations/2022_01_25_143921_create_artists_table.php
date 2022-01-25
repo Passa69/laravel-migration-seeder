6faa849f879c2f8acac784081789b6be0808058f
tommaso <passarotto13@gmail.com>
2022-01-25 15:46:33 +0100

    creazione tabella artists

--- MyLaravelMigration/database/migrations/2022_01_25_143921_create_artists_table.php
+++ MyLaravelMigration/database/migrations/2022_01_25_143921_create_artists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateArtistsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('artists', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->string('name', 10);
+            $table->string('surname', 15);
+            $table->date('date_of_birth');
+            $table->year('year_of_debut');
+            $table->string('name_band', 20) -> nullable();
+
             $table->timestamps();
         });
     }
@@ -31,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('artists');
     }
 }
