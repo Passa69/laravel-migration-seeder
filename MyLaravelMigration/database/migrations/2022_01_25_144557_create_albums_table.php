c8d99a0882e8ae962670c9ee2260f5d1e8a19629
tommaso <passarotto13@gmail.com>
2022-01-25 15:50:59 +0100

    creazione tabella albums

---
+++ MyLaravelMigration/database/migrations/2022_01_25_144557_create_albums_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name_album', 20);
+            $table->text('description') -> nullable();
+            $table->date('date_of_release');
+            $table->integer('number_of_listeners');
+            $table->integer('number_of_copies_sold');
+            
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
